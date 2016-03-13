d546e17bf58edd8b12891efa539abcb3e3a1f0bf
fraserxu <xvfeng123@gmail.com>
2016-03-13 20:25:29 +1100

    Updates

--- bundle.js
+++ bundle.js
@@ -4,8 +4,9 @@ var bel = require('bel')
 
 function render () {
   var opts = {
-    poster: 'sources/rides.png',
-    mp4Src: 'sources/rides.mp4'
+    width: '100%',
+    poster: 'http://7xrtnk.com1.z0.glb.clouddn.com/rides-poster.png',
+    mp4Src: 'http://7xrtnk.com1.z0.glb.clouddn.com/rides.mp4'
   }
   return bel`${Video(opts)}`
 }
